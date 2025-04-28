b46161f06026f1b715f1b1588c8c6e4b337c6ec7
IM23a-spirgif <spirgif@bzz.ch>
2025-04-28 09:14:57 +0200

    Changed name of render handler and small patch

--- src/main/java/net/fretux/knockedback/client/RenderCarryPassengerHandler.java
+++ src/main/java/net/fretux/knockedback/client/RenderCarryPassengerHandler.java
@@ -1,25 +1,24 @@
 package net.fretux.knockedback.client;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.CameraType;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = "knockedback", value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class ClientCarryRenderHandler {
+public class RenderCarryPassengerHandler {
     @SubscribeEvent
-    public static void onRenderPlayerPre(RenderPlayerEvent.Pre event) {
+    public static void onRenderLivingPre(RenderLivingEvent.Pre<LivingEntity, ?> evt) {
         Minecraft mc = Minecraft.getInstance();
-        LocalPlayer camera = mc.player;
-        if (camera == null) return;
         if (mc.options.getCameraType() != CameraType.FIRST_PERSON) return;
-        if (event.getEntity() instanceof LocalPlayer passenger
-                && passenger.getVehicle() == camera) {
-            event.setCanceled(true);
+        LivingEntity entity = evt.getEntity();
+        if (entity instanceof Player passenger
+                && passenger.getVehicle() == mc.player) {
+            evt.setCanceled(true);
         }
     }
 }
